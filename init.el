1120b9b0748ff99f5b54c7cf310326057647a37f
theFoodle <goosetherumfoodle@gmail.com>
2015-07-10 15:28:23 -0400

    fix emacs-lisp-mode hooks

--- init.el
+++ init.el
@@ -71,10 +71,10 @@
 
 ;; emacs-lisp mode
 ;; (setq emacs-lisp-mode-hook (cons emacs-lisp-mode-hook '(smartparens-mode show-paren-mode eldoc-mode rainbow-delimiters-mode)))
-(add-hook 'emacs-lisp-mode 'smartparens-mode)
-(add-hook 'emacs-lisp-mode 'show-paren-mode)
-(add-hook 'emacs-lisp-mode 'eldoc-mode)
-(add-hook 'emacs-lisp-mode 'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
 ;; clojure/cider mode hooks
 (add-hook 'cider-mode-hook 'eldoc-mode)
