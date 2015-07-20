76c6b623451fdfaa2bfcafe36e99c788bc21e796
theFoodle <goosetherumfoodle@gmail.com>
2015-07-20 11:31:46 -0400

    fixes smartparens-mode config

--- smartparens.el
+++ smartparens.el
@@ -1,4 +1,7 @@
-;; map slurping and barfing (because the default
-;; C-M-<right>/<left> were being capture by the terminal
-(define-key smartparens-mode-map (kbd "M-<right>") 'sp-backward-barf-sexp)
-(define-key smartparens-mode-map (kbd "M-<left>") 'sp-backward-slurp-sexp)
+
+(defun my-smartparens-mode-config ()
+  "map slurping and barfing (because the default C-M-<right>/<left> were being capture by the terminal)"
+  (local-set-key (kbd "M-<right>") 'sp-backward-barf-sexp)
+  (local-set-key (kbd "M-<left>") 'sp-backward-slurp-sexp))
+
+(add-hook 'smartparens-mode-hook 'my-smartparens-mode-config)
