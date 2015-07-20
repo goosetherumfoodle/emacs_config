582b1b552321eefcb80b8de2aa472d08ce8c851f
theFoodle <goosetherumfoodle@gmail.com>
2015-07-20 04:42:45 -0400

    add xah lee suggested hook to auto-byte compile elisp files when an .elc file already exists

--- elisp.el
+++ elisp.el
@@ -4,3 +4,14 @@
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+
+;; from: http://ergoemacs.org/emacs/emacs_byte_compile.html
+(defun byte-compile-current-buffer ()
+  "`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
+  (interactive)
+  (when (and (eq major-mode 'emacs-lisp-mode)
+             (file-exists-p (byte-compile-dest-file buffer-file-name)))
+    (byte-compile-file buffer-file-name)))
+
+(add-hook 'after-save-hook 'byte-compile-current-buffer)
+
