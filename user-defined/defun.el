433c2210345238ff13a6cda6b93dd900fd9bcaf7
theFoodle <goosetherumfoodle@gmail.com>
2015-07-20 11:49:32 -0400

    moves function from elisp.el to defun.el

--- user-defined/defun.el
+++ user-defined/defun.el
@@ -5,3 +5,11 @@
     (move-end-of-line nil)
     (newline)
     (indent-for-tab-command))
+
+;; from: http://ergoemacs.org/emacs/emacs_byte_compile.html
+(defun byte-compile-current-buffer ()
+  "`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
+  (interactive)
+  (when (and (eq major-mode 'emacs-lisp-mode)
+             (file-exists-p (byte-compile-dest-file buffer-file-name)))
+    (byte-compile-file buffer-file-name)))
--- user-defined/elisp.el
+++ user-defined/elisp.el
@@ -5,13 +5,6 @@
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
-;; from: http://ergoemacs.org/emacs/emacs_byte_compile.html
-(defun byte-compile-current-buffer ()
-  "`byte-compile' current buffer if it's emacs-lisp-mode and compiled file exists."
-  (interactive)
-  (when (and (eq major-mode 'emacs-lisp-mode)
-             (file-exists-p (byte-compile-dest-file buffer-file-name)))
-    (byte-compile-file buffer-file-name)))
-
+;; automatically byte-compile any .el file that already
+;; has an .elc file
 (add-hook 'after-save-hook 'byte-compile-current-buffer)
-
