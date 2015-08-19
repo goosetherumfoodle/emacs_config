d21cf9761c7dc55720fb4a124a742d07ea3fab67
theFoodle <goosetherumfoodle@gmail.com>
2015-08-19 12:22:29 -0400

    adds function to insert pry in ruby mode

--- user-defined/my-ruby.el
+++ user-defined/my-ruby.el
@@ -4,7 +4,12 @@
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
 (defun my-enh-ruby-mode-config ()
-  (local-set-key (kbd "C-o") 'jump-to-newline))
+  (local-set-key (kbd "C-o") 'jump-to-newline)
+  (fset 'insert-pry
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ("require 'pry'; binding.pry" 0 "%d")) arg))))
+
+
+
 
 (add-hook 'enh-ruby-mode-hook 'my-enh-ruby-mode-config)
 ;; ruby-mode
