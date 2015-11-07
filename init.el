9a217f784ac7bf4ccf220d257ad3d745465b5b68
theFoodle <goosetherumfoodle@gmail.com>
2015-11-07 02:13:35 -0500

    adds ssl to package manager connection

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
 ;; configure emacs package manager to include melpa packages
 (require 'package)
 (add-to-list 'package-archives
-  '("melpa" . "http://melpa.org/packages/") t)
+  '("melpa" . "https://melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;;For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
